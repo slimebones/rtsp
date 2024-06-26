3f67aa13366d198120966888f21b3a22196658f7
ryzhovalex <thed4rkof@gmail.com>
2024-06-26 16:45:02 +0300

    update CHANGELOG.md, poetry.lock, pyproject.toml, ...

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.2.0 - UNRELEASED
+
+- added "save" and "show" commands
--- rtsp/__main__.py
+++ rtsp/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+import climage
 import asyncio
 from datetime import datetime
 from pathlib import Path
@@ -6,16 +7,23 @@ import time
 from PIL import Image
 
 import cv2
+from pykit.res import Res
 
 from rtsp.saver import VideoSaver
 
 RUN_ACTIONS = [
     "save",
+    "show"
 ]
 
 SAVE_ACTION = [
     "video",
     "frame"
+]
+
+SHOW_ACTION = [
+    "window",
+    "console"
 ]
 
 def save_frame(url: str, out: Path, period: float):
@@ -32,41 +40,75 @@ def save_frame(url: str, out: Path, period: float):
     cap.release()
     cv2.destroyAllWindows()
 
-def show_video(url: str):
+def show_window_frame(url: str, period: float):
     cap = cv2.VideoCapture(url)
     while cap.isOpened():
         ret, frame = cap.read()
         if ret:
-            print(f"({datetime.now().strftime("%H:%M:%S")}) show img")
+            print(f"({datetime.now().strftime("%H:%M:%S")}) show frame")
             cv2.imshow("frame", frame)
         if cv2.waitKey(20) & 0xFF == ord("q"):
             break
+        if period > 0.0:
+            time.sleep(period)
     cap.release()
     cv2.destroyAllWindows()
 
-def main():
-    parser = argparse.ArgumentParser()
-    action_subparsers = parser.add_subparsers(dest="action", required=True, )
+async def show_console_frame(url: str, period: float):
+    reader = ContiniousVideoReader(url)
+    reader.start()
+    if period < 0.0:
+        period = 0.0
+    while reader.is_opened():
+        frame = reader.flush_frame().unwrap_or(None)
+        if frame is not None:
+            print(f"({datetime.now().strftime("%H:%M:%S")}) show frame")
+            img = Image.fromarray(frame, "RGB")
+            console_out = climage.convert_pil(img, is_unicode=True)
+            print(console_out)
+        # sleep in any case to give space in event loop
+        await asyncio.sleep(period)
 
-    save_action_parser = action_subparsers.add_parser("save")
+def _add_save_parsers(parser: argparse.ArgumentParser):
+    subparser = parser.add_subparsers(dest="save_action")
 
-    save_action_subparsers = save_action_parser.add_subparsers(dest="save_action")
-
-    video_save_parser = save_action_subparsers.add_parser("video")
-    video_save_parser.add_argument("url", type=str)
-    video_save_parser.add_argument(
+    video = subparser.add_parser("video")
+    video.add_argument("url", type=str)
+    video.add_argument(
         "--creds",
         type=str,
         dest="creds",
         required=True,
         help="creds in format <username>:<password>")
 
-    frame_save_parser = save_action_subparsers.add_parser("frame")
-    frame_save_parser.add_argument("url", type=str)
-    frame_save_parser.add_argument(
+    frame = subparser.add_parser("frame")
+    frame.add_argument("url", type=str)
+    frame.add_argument(
         "-o --out", type=Path, dest="out", default=Path("var/frame.png"))
-    frame_save_parser.add_argument(
-        "--delay", type=float, dest="delay", default=1.0)
+    frame.add_argument(
+        "-p --period", type=float, dest="period", default=1.0)
+
+def _add_show_parsers(parser: argparse.ArgumentParser):
+    subparser = parser.add_subparsers(dest="show_action")
+
+    window = subparser.add_parser("window")
+    window.add_argument("url", type=str)
+    window.add_argument(
+        "-p --period", type=float, dest="period", default=1.0)
+
+    console = subparser.add_parser("console")
+    console.add_argument("url", type=str)
+    console.add_argument(
+        "-p --period", type=float, dest="period", default=1.0)
+
+async def main():
+    parser = argparse.ArgumentParser()
+    action_subparsers = parser.add_subparsers(dest="action", required=True, )
+
+    save_action_parser = action_subparsers.add_parser("save")
+    _add_save_parsers(save_action_parser)
+    show_action_parser = action_subparsers.add_parser("show")
+    _add_show_parsers(show_action_parser)
 
     args = parser.parse_args()
 
@@ -76,9 +118,15 @@ def main():
                 case "video":
                     url = args.url
                     username, password = args.creds.split(":")
-                    asyncio.run(VideoSaver().save(url, username, password))
+                    await VideoSaver().save(url, username, password)
                 case "frame":
-                    save_frame(args.url, args.out, args.delay)
+                    save_frame(args.url, args.out, args.period)
+        case "show":
+            match args.show_action:
+                case "window":
+                    show_window_frame(args.url, args.period)
+                case "console":
+                    await show_console_frame(args.url, args.period)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
